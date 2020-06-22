57d26e466fb791efc83029d9576ae93d09dd0b19
EvolveD <jrhemann@gmail.com>
2020-06-22 06:49:32 -0500

    initial push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Grid from "./Components/Grid"
+
+
+function App() {
+  return (
+    <div>
+        <h1>Game of Life</h1>
+        <Grid></Grid>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Grid.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react"
+
+
+const Grid = () => {
+    const [grid, setGrid] = useState()
+
+
+return(
+
+    <div>
+
+    </div>
+)
+
+}
+
+
+export default Grid
