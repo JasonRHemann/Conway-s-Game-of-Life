ec0f8b75906ab38564128d3b95af6a9adf4d9eb6
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-06-26 10:23:41 -0500

    Finished MVP and Stretch

--- src/App.js
+++ src/App.js
@@ -6,15 +6,56 @@ const Layout = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  background-color: #33caff;
+  height: 100vh;
+`;
+
+const ContainerRules = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const Header = styled.h1`
+  font-size: 40px;
+`;
+
+const Rules = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: right;
+  margin-left: 30px;
 `;
 
 function App() {
   return (
     <Layout>
-      <h1>Game of Life</h1>
-      <Layout>
-        <Grid></Grid>
-      </Layout>
+      <Header>Game of Life</Header>
+      <ContainerRules>
+        <Layout>
+          <Grid></Grid>
+        </Layout>
+        <Rules>
+          <h2>Rules</h2>
+          <ol>
+            <li>
+              {" "}
+              Any live cell with fewer than two live neighbours dies, as if by
+              underpopulation.
+            </li>
+            <li>
+              Any live cell with two or three live neighbours lives on to the
+              next generation.{" "}
+            </li>
+            <li>
+              Any live cell with more than three live neighbours dies, as if by
+              overpopulation.{" "}
+            </li>
+            <li>
+              Any dead cell with exactly three live neighbours becomes a live
+              cell, as if by reproduction
+            </li>
+          </ol>
+        </Rules>
+      </ContainerRules>
     </Layout>
   );
 }
--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,70 +1,228 @@
-import React, { useState, useCallback, useRef } from "react";
+import React, { useState, useCallback, useRef, useEffect } from "react";
 import produce from "immer";
+import styled from "styled-components";
+import ReactTooltip from "react-tooltip";
+import { Tooltip } from "reactstrap";
 
-// Set the number of rows and columns.  Make this adjustable later
-const numRows = 25;
-const numCols = 25;
+const MainContainer = styled.div`
+  background-color: #33ff86;
+  border-radius: 5px;
+`;
 
-const Grid = () => {
-  // Creating a grid and grid state and looping over it setting all initial values to 0
-  const [grid, setGrid] = useState(() => {
-    const rows = [];
-    for (var i = 0; i < numRows; i++) {
-      rows.push(Array.from(Array(numCols), () => 0));
-    }
+const ButtonContainer = styled.div`
+  border; 2px solid red;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  padding: 5px;
+`;
 
-    return rows;
-  });
+const Button = styled.button`
+  color: black;
+  margin-top: 10px;
+  width: 8vw;
+  height: 40px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  font-weight: bolder;
+  background-color: #add8e6;
+`;
 
-  const [running, setRunning] = useState(false);
+const Counters = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+var _ = require("lodash");
+
+//setting list of operations to every neighbor
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+];
+
+function Grid() {
+  //setting variables and state.
+  const [running, setrunning] = useState(false);
   const runningRef = useRef(running);
   runningRef.current = running;
+  const [generation, setGeneration] = useState(0);
+  const timeRef = useRef(200);
+  const [sum, setSum] = useState(0);
+  const numRows = 35;
+  const numCols = 80;
+  const colors = ["#427BFF", "#7044FF", "#FFDB2C", "#FFB92C"];
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+  const toggle = () => setTooltipOpen(!tooltipOpen);
 
-  // Simulation function
-  const runSimulation = useCallback(() => {
+  //creating grid and setting to all zeros
+  function initialState() {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    console.log(rows);
+    return rows;
+  }
+
+  const [grid, setGrid] = useState(initialState);
+
+  const runSim = useCallback(() => {
     if (!runningRef.current) {
       return;
     }
-    setTimeout(runSimulation, 1000);
-  }, []);
-
+    setGrid((g) => {
+      let validGrid = false;
+      validGrid = false;
+      // creat copy of grid
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              //checking to see if out of bounds.
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+            //counting number of neighbors
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              validGrid = true;
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+        // counting generation
+        if (validGrid) {
+          setGeneration((num) => num + 1);
+        }
+        //counting number of alive cells
+        setSum(
+          gridCopy.flat().reduce((acc, cv) => {
+            return acc + cv;
+          })
+        );
+      });
+    });
+    setTimeout(runSim, timeRef.current);
+  }, [timeRef.current]);
+  console.log(timeRef.current);
   return (
-    <div>
-      <button onClick={() => setRunning(!running)}>
-        {/* Togfgle start and stop */}
-        {running ? "Stop" : "Start"}
-      </button>
+    <MainContainer>
+      <ButtonContainer>
+        <Button
+          onClick={() => {
+            setrunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              runSim();
+            }
+          }}
+        >
+          {!runningRef.current ? "Start" : "Stop"}
+        </Button>
+
+        {/* set grid with random live cells */}
+        <Button
+          onClick={() => {
+            const rows = [];
+            for (let i = 0; i < numRows; i++) {
+              rows.push(
+                Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0))
+              );
+            }
+            setGrid(rows);
+          }}
+        >
+          Random
+        </Button>
+
+        {/* reset all values to initial state to clear */}
+        <Button
+          onClick={() => {
+            setGrid(initialState);
+            setGeneration(0);
+            setSum(0);
+          }}
+        >
+          Clear
+        </Button>
+
+        <Button
+          onClick={() => {
+            timeRef.current = 2;
+          }}
+        >
+          Fast
+        </Button>
+        <Button
+          onClick={() => {
+            timeRef.current = 200;
+          }}
+        >
+          Normal
+        </Button>
+        <Button
+          onClick={() => {
+            timeRef.current = 400;
+          }}
+        >
+          Slow
+        </Button>
+      </ButtonContainer>
+      <Counters>
+        <h2> Generation: {generation}</h2>
+        <h2> Cells: {sum}</h2>
+      </Counters>
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+          gridTemplateColumns: `repeat(${numCols},15px)`,
         }}
       >
+        {/* drawing grid */}
         {grid.map((rows, i) =>
-          rows.map((col, j) => (
+          rows.map((col, k) => (
             <div
-              key={`${i}-${j}`}
-              //Make grid clickable
+              key={`${i}-${k}`}
               onClick={() => {
-                //Setting grid to new copy so that it is mutable
+                //using immer to make a copy and utilize double buffer
                 const newGrid = produce(grid, (gridCopy) => {
-                  gridCopy[i][j] = grid[i][j] ? 0 : 1;
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
                 });
                 setGrid(newGrid);
               }}
-              //Setting width, height, and background color.  Make this editable later
               style={{
-                width: 20,
-                height: 20,
-                backgroundColor: grid[i][j] ? "black" : undefined,
+                width: 14,
+                height: 15,
+                //changing background depending on amount of cells
+                backgroundColor:
+                  grid[i][k] && sum > 200
+                    ? _.sample(colors)
+                    : grid[i][k] && sum > 100
+                    ? "#4666FF"
+                    : grid[i][k] && sum >= 0
+                    ? "#FE000D"
+                    : undefined,
                 border: "1px solid black",
               }}
             />
           ))
         )}
       </div>
-    </div>
+    </MainContainer>
   );
-};
-
+}
 export default Grid;
